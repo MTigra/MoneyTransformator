eb692e8b5b94ea0ec7302349bffeb0d994599dde
Tigran Martirosyan <tikol2008@gmail.com>
2020-06-05 04:49:28 +0300

    d

--- spring-boot-microservice-currency-conversion-service/builddeploy.sh
+++ spring-boot-microservice-currency-conversion-service/builddeploy.sh
@@ -1,5 +1,4 @@
 mvn clean package -DskipTests -Pcloud
-
 az spring-cloud app deploy \
   -n currency-service  \
   --jar-path target/currency-service-0.0.1-SNAPSHOT.jar
--- spring-boot-microservice-forex-service/builddeploy.sh
+++ spring-boot-microservice-forex-service/builddeploy.sh
@@ -1,5 +1,4 @@
 mvn clean package -DskipTests -Pcloud
-
 az spring-cloud app deploy \
   -n forex-service  \
   --jar-path target/forex-service-0.0.1-SNAPSHOT.jar
