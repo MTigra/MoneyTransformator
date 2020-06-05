b0405737cd395b1e0767713b14855df6762d4bf3
Tigran Martirosyan <tikol2008@gmail.com>
2020-06-05 07:40:46 +0300

    mm

--- unit-transformation-service/builddeploy.sh
+++ unit-transformation-service/builddeploy.sh
@@ -1,4 +1,4 @@
 mvn clean package -DskipTests -Pcloud
 az spring-cloud app deploy \
-  -n unit-transformation-service  \
-  --jar-path target/unit-transformation-service-0.0.1-SNAPSHOT.jar
+  -n unit-service  \
+  --jar-path target/unit-service-0.0.1-SNAPSHOT.jar
